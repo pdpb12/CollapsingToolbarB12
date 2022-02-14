665c4ed62733c9c9b25249a1ce38164bc6d63456
rustamsobirov <carwolt123@gmail.com>
2022-02-14 18:39:13 +0500

    Initial commit

---
+++ app/src/main/java/me/ruyeo/collapsingtoolbarb12/MainActivity.kt
@@ -0,0 +1,12 @@
+package me.ruyeo.collapsingtoolbarb12
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.android.material.appbar.CollapsingToolbarLayout
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
